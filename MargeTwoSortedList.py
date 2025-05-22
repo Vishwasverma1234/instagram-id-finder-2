83fd1d9fa2e60635eddedb044f49800ed07a5f54
Vishwasverma1234 <140061045+Vishwasverma1234@users.noreply.github.com>
2025-05-21 23:28:15 -0700

    this the mian file
    this is the mian file of the code dwe done for the our frojects

---
+++ MargeTwoSortedList.py
@@ -0,0 +1,14 @@
+def dublicate(arr):
+
+    for i in range(len(arr)):
+        for j in range(i+1(len(arr))):
+            if i[arr] == j[arr]:
+                del j 
+                return arr
+
+import array as arr
+
+arr = [ 1,1,2,2,3,3,4,5]
+
+
+print(dublicate(arr))
